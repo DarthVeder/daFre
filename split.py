5b6dbe92474ab485304101866937f3209fd64a33
Marco Gregorini <mm191274@gmail.com>
2017-04-23 18:54:34 +0200

    Added linux capability Added two test cases for split.py and writeXml.py

--- split.py
+++ split.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import unittest
 import re
 import writeXml
 import io
@@ -10,11 +11,16 @@ import os
 from mutagen.mp3 import MP3 
 
 def split(language, unit, page, dialogue_title, file_name, flag):
+    """
+    This function split a given text file following some syntacti rules and following a dictionary.
+    It also calls the synchornization library aeneas
+    """
+    
     # Storing the ordered name of the characters
-    name = collections.OrderedDict()
-    # text spoken by each character (name, line_number)
-    speech_text = ['null', 'null']
-    speech_line_spoken_by = []
+    character_name = collections.OrderedDict()
+    # text spoken by each character (character_name, line_number)
+    speech_text_lines      = ['null', 'null']
+    speech_line_spoken_by  = []
     path           = file_name['dir']
     dialogue_file  = file_name['oldtext']
     audio_file     = file_name['oldaudio']
@@ -24,76 +30,83 @@ def split(language, unit, page, dialogue_title, file_name, flag):
     logging.info('Splitting required')
     logging.info('Dialogue File: %s', dialogue_file)
     # input text
-    logging.debug('Path: %s; dialogue file: %s',path,dialogue_file)
+    logging.debug('Path: %s; dialogue file: %s', path, dialogue_file)
     # input text file with required base splitting
     fin = io.open(path + dialogue_file, encoding='utf-8', mode='r')
     # output file with new splitting based on dictionary, base splitting
     # and !?,;:.
-    out_file  = path + base_file_name + '_mysplit.txt'
-    file_mode = u'w'
-    fout      = io.open(out_file,encoding='utf-8', mode=file_mode)
+    out_file   = path + base_file_name + '_mysplit.txt'
+    file_mode  = u'w'
+    split_file = io.open(out_file, encoding='utf-8', mode=file_mode)
 
     # Variable used to check if the character name has been found and stored
-    name_is_set = False
-    num_line    = 2 # this variable stores each line number in the
-                    # fout file
+    character_name_is_set = False
+    num_line    = 2 # this variable stores each line number (starting from
+                    # 1) in the
+                    # fout file. The first line is reserved to the title,
+                    # this is why num_line is 2
 
     # Flag used to identify the first line of the fin file reserved
     # to the title
     set_mp3_title = False
-    for l in fin:
+    for l in fin:        
         if not set_mp3_title:
-            if l != '\n': # if title not a blank line
+            if l != '\n': # if title not a blank line, else do nothing
                 dialogue_title = l.strip('\n')
                 # writing dialogue title to file for mp3 synchronization   
-                fout.write(dialogue_title + '\n')                
+                split_file.write(dialogue_title + '\n')                
             set_mp3_title = True
-        elif l != '\n':
+        elif l != '\n' and l.find(r'\b') == -1:            
             word = l.split(' ')        
             ist = 0        
             # first word of the line is not a key and the name of who's talking
             # is not set
-            if word[0] not in name and not name_is_set:
+            if word[0] not in character_name and not character_name_is_set:
                 key         = word[0].strip(':') # name of who's speaking
-                name[key]   = [] # preparing for the phrase(s) that will be spoken                
-                name_is_set = True
+                character_name[key]   = [] # preparing for the phrase(s) that will be spoken                
+                character_name_is_set = True
                 ist         = 1        
-                speech_line_spoken_by.append((key,num_line))
+                speech_line_spoken_by.append((key,num_line))                
             # the first word is a name already present
-            elif word[0] in name:
-                name_is_set = True                    
+            elif word[0] in character_name:
+                character_name_is_set = True                    
                 key         = word[0].strip(':')                
                 ist         = 1
-                speech_line_spoken_by.append((key,num_line))
+                speech_line_spoken_by.append((key,num_line))                              
             # There is no name in the first word    
             else:
                 ist = 0            
             
             # splitting the phrase if required            
-            phrase = " ".join(word[ist:])
-            name[key].append(phrase.strip('\n'))
+            phrase = " ".join(word[ist:])            
+            character_name[key].append(phrase.strip('\n '))
             wd = phrase.split()        
             if len(wd) <= 2:
                 # By default 2 words are kept together
-                fout.write(phrase)
-                num_line = num_line + 1
-                speech_text.append(phrase.strip('\n'))
-            else:
+                split_file.write(phrase)                
+                speech_text_lines.append(phrase.strip('\n '))                                
+            else:                
                 # Building speech line by line, storing how many split per line per author
-                ck = re.split(r'(?<=[.,;?!:]) +',phrase.strip('\n'))
+                ck = re.split(r'(?<=[.,;?!:]) +', phrase.strip('\n '))                
                 for p in ck:
-                    cks = re.split(r' \b(?=\bet)+',p)
+                    cks = re.split(r' \b(?=\bet)+', p)                    
                     for cksi in cks:
-                        fout.write(cksi+'\n')
+                        split_file.write(cksi+'\n')                        
+                        speech_text_lines.append(cksi.strip('\n '))                        
                         num_line = num_line + 1
-                        speech_text.append(cksi.strip('\n'))
+        elif l.find(r'\b') != -1:            
+            comment_text = l.strip('\n')
+            speech_text_lines.append(comment_text)            
                 
         else:
-            name_is_set = False
+            character_name_is_set = False
+        
 
     fin.close()
-    fout.close()
-    for k in name.keys():
+    split_file.close()
+
+    
+    for k in character_name.keys():
         logging.debug('Character %s, type unicode %s', k, isinstance(k,unicode))
 
     # Finding duration (s) of audio file:    
@@ -126,43 +139,97 @@ def split(language, unit, page, dialogue_title, file_name, flag):
     # Binding time to speaker
     text_to_print = []    
     logging.debug('EXCHANGE START')        
-    for i in range(len(speech_line_spoken_by) ):
+    for i in range(len(speech_line_spoken_by) ):        
         ch  = speech_line_spoken_by[i][0]
         idx = speech_line_spoken_by[i][1]
         logging.debug('Speaker: %s',ch)
         if i != len(speech_line_spoken_by) - 1:
             idx1 = speech_line_spoken_by[i+1][1]
         else:
-            idx1 = len(speech_text)
-        text = [ t_start[idx-2:idx1-2] , speech_text[idx:idx1] ]            
+            idx1 = len(speech_text_lines)        
+        text = [ t_start[idx-2:idx1-2] , speech_text_lines[idx:idx1] ]            
         text_to_print.append([ch] + text)
 
     logging.debug('EXCHANGE STOP')
-    
+
     # Writing xml file
     logging.debug('Writing XML file')   
     out_file = path + base_file_name + '.xml'
     logging.debug('XML file: %s', out_file)
     writeXml.writeXml(language, unit, dialogue_title, page, new_audio_file, \
-                      tend_s, name.keys(), text_to_print, out_file)
+                      tend_s, character_name.keys(), text_to_print, out_file)
 
 
-if __name__ == '__main__':
-    unit = u'3'
-    page = u'Page 56'
-    dialogue_title = 'Univers perso'
-    base_file_name = u'u3_canulli_p56'
-    dialogue_file  = base_file_name + u'.txt'
-    old_audio_file = base_file_name + u'.mp3'
-    new_audio_file = u'v1u3_canulli_p56.mp3'
+class SplitTest(unittest.TestCase):
+    def test_splitting_text(self):
+        unit = u'3'
+        page = u'Page 56'
+        dialogue_title = 'Univers perso'
+        base_file_name = u'u3_canulli_p56'
+        dialogue_file  = base_file_name + u'.txt'
+        old_audio_file = base_file_name + u'.mp3'
+        new_audio_file = u'v1u3_canulli_p56.mp3'
 
-    file_name = {}
-    file_name['dir']      = u'./testSplit/'
-    file_name['oldaudio'] = old_audio_file
-    file_name['newaudio'] = new_audio_file
-    file_name['oldtext']  = dialogue_file
-    file_name['file']     = old_audio_file.split('.')[0]
+        file_name             = {}
+        file_name['dir']      = u'./testSplit/'
+        file_name['oldaudio'] = old_audio_file
+        file_name['newaudio'] = new_audio_file
+        file_name['oldtext']  = dialogue_file
+        file_name['file']     = old_audio_file.split('.')[0]
     
-    flag = 'sync' 
+        flag = 'sync' 
 
-    split('fra', unit, page, dialogue_title, file_name, flag)
+        split('fra', unit, page, dialogue_title, file_name, flag)
+
+        rel_path_file = u'.' + os.sep + u'testSplit' + os.sep
+        comparisonFile = rel_path_file + u'u3_canulli_p56_mysplit.sol'
+        testFile      = rel_path_file + u'u3_canulli_p56_mysplit.txt'
+        
+        import difflib
+
+        diff = difflib.context_diff(io.open(testFile, encoding='utf-8', mode='r').readlines(), \
+                             io.open(comparisonFile, encoding='utf-8', mode='r').readlines()  )
+        it = 0
+        for el in diff:
+            it = it + 1
+
+        print it
+        self.assertEqual(it, 0, 'Split file not equal to comparison sol file')
+
+    def test_comments(self):
+        unit = u'1'
+        page = u'Page 63'
+        dialogue_title = 'Anonymous from Beowulf The Coming of Beowulf '
+        base_file_name = u'u1_beowulf_p63'
+        dialogue_file  = base_file_name + u'.txt'
+        old_audio_file = base_file_name + u'.mp3'
+        new_audio_file = u'v1u1_cattaneo_p63.mp3'
+
+        file_name             = {}
+        file_name['dir']      = u'./testSplit/'
+        file_name['oldaudio'] = old_audio_file
+        file_name['newaudio'] = new_audio_file
+        file_name['oldtext']  = dialogue_file
+        file_name['file']     = old_audio_file.split('.')[0]
+    
+        flag = 'sync' 
+
+        split('eng', unit, page, dialogue_title, file_name, flag)
+
+        rel_path_file = u'.' + os.sep + u'testSplit' + os.sep
+        comparisonFile = rel_path_file + u'u1_beowulf_p63_xml.sol'
+        testFile      = rel_path_file + u'u1_beowulf_p63.xml'
+        
+        import difflib
+
+        diff = difflib.context_diff(io.open(testFile, encoding='utf-8', mode='r').readlines(), \
+                             io.open(comparisonFile, encoding='utf-8', mode='r').readlines()  )
+        it = 0
+        for el in diff:
+            it = it + 1
+
+        print it
+        self.assertEqual(it, 0, 'Split comment xml file not equal to comparison xml file')        
+        
+if __name__ == '__main__':
+    unittest.main()
--- writeXml.py
+++ writeXml.py
@@ -8,8 +8,10 @@ import logging
 unit_text      = { 'fra': u'Unité ', 'eng': u'Unit '}
 character_text = { 'fra': u'Teller', 'eng': u'Speaker' }
 character_id   = string.lowercase[:26]
+xml_language   = { 'fra' : u'fr', 'eng': u'en'}
 
-def writeXml(language, unit, dialogue_title, page, audio_file, tend, character, text_to_print, out_file):
+def writeXml(language, unit, dialogue_title, page, audio_file, \
+             tend, character, text_to_print, out_file):
     name_to_id = {}
     root       = ET.Element('karaoke_config')
     b          = ET.SubElement(root, 'book_info')
@@ -24,7 +26,7 @@ def writeXml(language, unit, dialogue_title, page, audio_file, tend, character, 
     a      = ET.SubElement(k, 'audio')
     a.text = audio_file
     l      = ET.SubElement(k, 'lang')
-    l.text = language
+    l.text = xml_language[language]
 
     c = ET.SubElement(k, 'characters')
 
@@ -44,18 +46,35 @@ def writeXml(language, unit, dialogue_title, page, audio_file, tend, character, 
         idx            = idx + 1
 
     s = ET.SubElement(k, 'subtitles')
-    
-    for i in range(0,len(text_to_print)):
+
+    for i in range(0,len(text_to_print)):        
         lr = ET.SubElement(s, 'line')
         lr.attrib['ref'] = name_to_id[text_to_print[i][0]]
         time = text_to_print[i][1][:]
-        phrase = text_to_print[i][2][:]
-        for j in range(0,len(time)):            
-            tmp                 = float(time[j])            
-            time_string         = ' %.6f ' % tmp
-            cue                 = ET.SubElement(lr, 'cue')
-            cue.attrib['start'] = time_string            
-            cue.text            = phrase[j]    
+        phrase = text_to_print[i][2][:]                
+
+        itime = 0
+        for j in range(0,len(phrase)):
+            is_comment = False
+            if phrase[j].find(r'\b') != -1:
+                is_comment = True                
+                
+            if not is_comment:
+                tmp                 = float(time[itime])            
+                time_string         = ' %.6f ' % tmp
+                cue                 = ET.SubElement(lr, 'cue')
+                cue.attrib['start'] = time_string            
+                cue.text            = phrase[j]
+                itime = itime + 1
+            elif is_comment:
+                comment_text = phrase[j].split(r'\b')[1]                
+                if comment_text.replace(' ','') == '':
+                    comment_text = u'<br />'
+                    
+                cue      = ET.SubElement(lr, 'print_html')                
+                cue.text = ET.CDATA(comment_text) 
+                
+            
 
     cue = ET.SubElement(s, 'cue')
     cue.attrib['start'] = '%.6f' % tend
@@ -65,14 +84,4 @@ def writeXml(language, unit, dialogue_title, page, audio_file, tend, character, 
     tree.write(out_file, pretty_print=True, encoding='utf-8')        
 
 if __name__ == '__main__':
-    unit = u'3'
-    dialogue_title = u'Tu vas au cours de piano? - Dialogue 2'
-    page = u'Page 57'
-    audio_file = u'v1u3_echanges_p57.mp3'
-    character = ['Nathan', 'Abdou']
-    out_file = 'myxml.xml'
-    text_to_print = []
-    text_to_print.append(['Nathan', ['8.800'], ['Salut Abdou!']])
-    text_to_print.append(['Abdou',  ['10.160', '11.120'], ['Salut Nathan!', '\xc3\x87a va?']])
-    
-    writeXml('fr', unit, dialogue_title, page, audio_file, character, text_to_print, out_file)
+    pass
